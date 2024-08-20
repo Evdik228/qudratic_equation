c68feb956fb53ee68e592e59487a7e568209234b
smth <smth>
2024-08-20 18:09:15 +0300

    initial commit

---
+++ lesson1/app.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <math.h>
+
+enum roots {
+    NO_ROOT   = 0,
+    ONE_ROOT  = 1,
+    TWO_ROOTS = 2,
+    INF_ROOTS = 3,
+}
+
+/*function initialization*/
+int SolvesSquare(double caf_one, double caf_two, double caf_three, double *x1, double *x2) { // rename
+    const double ZERO = 0.000001;
+    double diskr = 0; // rename
+    if (fabs(caf_one) <= ZERO) {
+        if (fabs(caf_two) <= ZERO) {
+            return (fabs(caf_three) <= ZERO) ? INF_ROOTS : 0;      
+        } else {
+            *x1 = -caf_three /caf_two; 
+        }
+    } else {
+        diskr = (caf_two *caf_two -4 * caf_one * caf_three); 
+        return ONE_ROOT;
+    }
+
+    if (diskr <= ZERO) { 
+        return NO_ROOT; 
+    }
+    else if (diskr == 0) {
+        *x1 = -caf_two / (2 * caf_one);
+        return ONE_ROOT;
+    } else {                                      
+        *x1 = (-caf_two - sqrt(diskr)) / (2 * caf_one);
+        *x2 = (-caf_two + sqrt(diskr)) / (2 * caf_one);
+        return TWO_ROOTS;
+    }
+}
+
+int data_output(int n_roots, double x1, double x2){
+    switch (n_roots)
+    {
+    case NO_ROOT: printf("No roots \n"); 
+        break;
+    case ONE_ROOT: printf("One root: %.2f", x1);
+        break;
+    case TWO_ROOTS: printf("First root:%.2f, Second root:%.2f", x1, x2);
+        break;
+    case INF_ROOTS: printf("infinity roots");
+        break;
+    default: printf("ERROR, another number of roots:%i", n_roots);
+        break;
+    }
+    return 0;
+}
+
+int data_entry(double * caf_one, double * caf_two, double * caf_three) {
+    
+    printf("Enter rational numbers for first сoefficient,\nsecond coefficient, and third coefficient:"); 
+    
+    double c_one = 0;
+    double c_two = 0;
+    double c_three = 0;
+    int correct = 0;
+
+    while (correct != 3) {
+
+        correct = scanf ("%lg, %lg, %lg", c_one, c_two, c_three);
+
+        if (correct != 3){
+            printf("You have an input error, please enter the correct values!")
+        }
+    }
+     
+     caf_one = c_one;
+     caf_two = c_two;
+     caf_three = c_three;
+        
+}
+
+int main()
+{   
+    /*initialization and entering variables*/
+
+    double caf_one = 0,caf_two = 0, caf_three = 0;          
+    double x1 = 0, x2 = 0;
+
+    /*сalling function*/
+
+    data_entry(&caf_one, &caf_two, &caf_three);          
+
+    int n_roots = SolvesSquare(caf_one,caf_two, caf_three, &x1 , &x2);
+
+    data_output(n_roots, x1, x2);
+
+    return 0;
+}
