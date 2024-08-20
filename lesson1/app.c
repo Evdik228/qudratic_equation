ea5c1eba5adbf6a6278ea9dd4eb75090f4c198ca
smth <smth>
2024-08-20 19:45:34 +0300

    1

--- lesson1/app.c
+++ lesson1/app.c
@@ -6,32 +6,33 @@ enum roots {
     ONE_ROOT  = 1,
     TWO_ROOTS = 2,
     INF_ROOTS = 3,
-}
+};
 
 /*function initialization*/
-int SolvesSquare(double caf_one, double caf_two, double caf_three, double *x1, double *x2) { // rename
+int SolvesSquare(double cuef_one, double cuef_two, double cuef_three, double *x1, double *x2) { 
     const double ZERO = 0.000001;
-    double diskr = 0; // rename
-    if (fabs(caf_one) <= ZERO) {
-        if (fabs(caf_two) <= ZERO) {
-            return (fabs(caf_three) <= ZERO) ? INF_ROOTS : 0;      
+    double discriminant = 0; 
+
+    if (fabs(cuef_one) <= ZERO) {
+        if (fabs(cuef_two) <= ZERO) {
+            
+            return (fabs(cuef_three) <= ZERO) ? INF_ROOTS : 0;      
         } else {
-            *x1 = -caf_three /caf_two; 
+            *x1 = -cuef_three /cuef_two; 
         }
     } else {
-        diskr = (caf_two *caf_two -4 * caf_one * caf_three); 
-        return ONE_ROOT;
+        discriminant = (cuef_two *cuef_two - 4 * cuef_one * cuef_three); 
     }
 
-    if (diskr <= ZERO) { 
+    if (discriminant <= ZERO) { 
         return NO_ROOT; 
     }
-    else if (diskr == 0) {
-        *x1 = -caf_two / (2 * caf_one);
+    else if (discriminant == 0) {
+        *x1 = -cuef_two / (2 * cuef_one);
         return ONE_ROOT;
     } else {                                      
-        *x1 = (-caf_two - sqrt(diskr)) / (2 * caf_one);
-        *x2 = (-caf_two + sqrt(diskr)) / (2 * caf_one);
+        *x1 = (-cuef_two - sqrt(discriminant)) / (2 * cuef_one);
+        *x2 = (-cuef_two + sqrt(discriminant)) / (2 * cuef_one);
         return TWO_ROOTS;
     }
 }
@@ -41,39 +42,40 @@ int data_output(int n_roots, double x1, double x2){
     {
     case NO_ROOT: printf("No roots \n"); 
         break;
-    case ONE_ROOT: printf("One root: %.2f", x1);
+    case ONE_ROOT: printf("One root: %.2f\n", x1);
         break;
-    case TWO_ROOTS: printf("First root:%.2f, Second root:%.2f", x1, x2);
+    case TWO_ROOTS: printf("First root:%.2f, Second root:%.2f\n", x1, x2);
         break;
-    case INF_ROOTS: printf("infinity roots");
+    case INF_ROOTS: printf("infinity roots\n");
         break;
-    default: printf("ERROR, another number of roots:%i", n_roots);
+    default: printf("ERROR, another number of roots:%i\n", n_roots);
         break;
     }
     return 0;
 }
 
-int data_entry(double * caf_one, double * caf_two, double * caf_three) {
+int data_entry(double * cuef_one, double * cuef_two, double * cuef_three) {
     
-    printf("Enter rational numbers for first сoefficient,\nsecond coefficient, and third coefficient:"); 
-    while (scanf ("%lg, %lg, %lg", c_one, c_two, c_three) != 3) {
-        printf("You have an input error, please enter the correct values!");
-    }
-        
+    printf("Enter the coefficients of a quadratic equation of the form ax^2 + b^x +c :"); 
+    while (scanf ("%lg %lg %lg", cuef_one, cuef_two, cuef_three) != 3) {
+        printf("You have an input error, please enter the correct values!\n");
+        printf("Enter the coefficients of a quadratic equation of the form ax^2 + b^x +c :");
+}
+    return 0;
 }
 
 int main()
 {   
     /*initialization and entering variables*/
 
-    double caf_one = 0,caf_two = 0, caf_three = 0;          
+    double cuef_one = 0,cuef_two = 0, cuef_three = 0;          
     double x1 = 0, x2 = 0;
 
     /*сalling function*/
 
-    data_entry(&caf_one, &caf_two, &caf_three);          
+    data_entry(&cuef_one, &cuef_two, &cuef_three);      
 
-    int n_roots = SolvesSquare(caf_one,caf_two, caf_three, &x1 , &x2);
+    int n_roots = SolvesSquare(cuef_one,cuef_two, cuef_three, &x1 , &x2);
 
     data_output(n_roots, x1, x2);
 
